9dbc351833389fd1a9fe047b8f36d8c0d2d5808d
Paul Graham <paul.graham@coprime.co.uk>
2022-10-13 15:21:11 +0100

    added test project and removed old lite project

---
+++ src/SFA.DAS.TrackProgress.Jobs.Tests/WhenNewProgressHasBeenAdded.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using AutoFixture.NUnit3;
+using Moq;
+using NServiceBus.Testing;
+using SFA.DAS.TrackProgress.Jobs.Api;
+using SFA.DAS.TrackProgress.Jobs.Handlers;
+using SFA.DAS.TrackProgress.Messages.Events;
+
+namespace SFA.DAS.TrackProgress.Jobs.Tests;
+
+public class WhenNewProgressHasBeenAdded
+    {
+    [Test, AutoMoqData]
+    public async Task Then_notify_apim(
+        [Frozen] Mock<ITrackProgressOuterApi> api,
+        NewProgressAddedEventHandler sut,
+        NewProgressAddedEvent evt)
+    {
+        await sut.Handle(evt, new TestableMessageHandlerContext());
+
+        api.Verify(m => m.CreateSnapshot(evt.CommitmentsApprenticeshipId));
+    }
+}
