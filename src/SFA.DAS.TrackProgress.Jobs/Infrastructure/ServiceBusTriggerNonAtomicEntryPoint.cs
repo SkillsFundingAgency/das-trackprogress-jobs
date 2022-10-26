20015d3fb566e7f468d76f5b5274fbf2ec526424
Paul Graham <paul.graham@coprime.co.uk>
2022-10-26 11:54:55 +0100

    and without the entry point

--- src/SFA.DAS.TrackProgress.Jobs/Infrastructure/ServiceBusTriggerNonAtomicEntryPoint.cs
+++ src/SFA.DAS.TrackProgress.Jobs/Infrastructure/ServiceBusTriggerNonAtomicEntryPoint.cs
@@ -1,23 +1,23 @@
-﻿using System.Threading.Tasks;
-using Azure.Messaging.ServiceBus;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Extensions.Logging;
-using NServiceBus;
+﻿//using System.Threading.Tasks;
+//using Azure.Messaging.ServiceBus;
+//using Microsoft.Azure.WebJobs;
+//using Microsoft.Extensions.Logging;
+//using NServiceBus;
 
-namespace SFA.DAS.TrackProgress.Jobs.Infrastructure;
+//namespace SFA.DAS.TrackProgress.Jobs.Infrastructure;
 
-internal class ServiceBusTriggerNonAtomicEntryPoint
-{
-    private readonly IFunctionEndpoint endpoint;
+//internal class ServiceBusTriggerNonAtomicEntryPoint
+//{
+//    private readonly IFunctionEndpoint endpoint;
 
-    public ServiceBusTriggerNonAtomicEntryPoint(IFunctionEndpoint endpoint) => this.endpoint = endpoint;
+//    public ServiceBusTriggerNonAtomicEntryPoint(IFunctionEndpoint endpoint) => this.endpoint = endpoint;
 
-    [FunctionName("TrackProcessJobs")]
-    public async Task Run(
-        [ServiceBusTrigger(queueName: QueueNames.TrackProgress, Connection = "AzureWebJobsServiceBus")] ServiceBusReceivedMessage message,
-        ILogger logger,
-        ExecutionContext context)
-    {
-        await endpoint.ProcessNonAtomic(message, context, logger);
-    }
-}
+//    [FunctionName("TrackProcessJobs")]
+//    public async Task Run(
+//        [ServiceBusTrigger(queueName: QueueNames.TrackProgress, Connection = "AzureWebJobsServiceBus")] ServiceBusReceivedMessage message,
+//        ILogger logger,
+//        ExecutionContext context)
+//    {
+//        await endpoint.ProcessNonAtomic(message, context, logger);
+//    }
+//}
