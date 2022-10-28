d359477176a1a84d59879547cc4f3d418ddea127
Paul Graham <paul.graham@coprime.co.uk>
2022-10-28 16:45:38 +0100

    corrected KSB logging

--- src/SFA.DAS.TrackProgress.Jobs/Handlers/CacheKsbsCommandHandler.cs
+++ src/SFA.DAS.TrackProgress.Jobs/Handlers/CacheKsbsCommandHandler.cs
@@ -21,7 +21,7 @@ public class CacheKsbsCommandHandler : IHandleMessages<CacheKsbsCommand>
 
     public async Task Handle(CacheKsbsCommand message, IMessageHandlerContext context)
     {
-        _logger.LogInformation("Started processing KSBs {name} for course standard {id}", nameof(NewProgressAddedEvent), message?.StandardUid);
+        _logger.LogInformation("Started processing KSBs {name} for course standard {id}", nameof(CacheKsbsCommand), message?.StandardUid);
         try
         {
             _logger.LogInformation("Processing KSBs for course standard {0}", message?.StandardUid);
@@ -29,7 +29,7 @@ public class CacheKsbsCommandHandler : IHandleMessages<CacheKsbsCommand>
         }
         catch (Exception e)
         {
-            _logger.LogError(e, "Error when processing {name} for course standard {id}", nameof(NewProgressAddedEvent), message?.StandardUid);
+            _logger.LogError(e, "Error when processing {name} for course standard {id}", nameof(CacheKsbsCommand), message?.StandardUid);
             throw;
         }
     }
